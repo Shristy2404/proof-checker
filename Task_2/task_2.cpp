d8e49f62293214785f9568a7779ffb314fb74504
Shristy2404 <shristykapoor24@gmail.com>
2018-10-19 23:44:46 +0530

    Added remove_brackets function

--- Task_2/task_2.cpp
+++ Task_2/task_2.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class proof_validator
 {
 public:
-	string str[];
+	std::vector<std::string> str;
 	int k;
-	void remove_brackets();
+	void remove_brackets( string rb);
 	proof_validator( int l);
 }; 
 
 proof_validator:: proof_validator(int l)
 {
 	k=l;
-	str[k];
-	cout << k << endl;
 }
 
-void remove_brackets()
+string remove_brackets(string rb)
 {
-  cout << "Working";
+	int len = rb.length();
+	string temp = rb.substr(1,len-2);
+	rb = temp;
+	rb.resize(len-2);
+	return rb;
 }
 
 int main()
 {
 	proof_validator obj(10);
-	//cout << str.length() << endl;
-	remove_brackets();
 	return 0;
 }
