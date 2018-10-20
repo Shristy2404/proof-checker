c4ed4aed4e2237fbfde4bdf2eedab05338d97c87
Shristy2404 <shristykapoor24@gmail.com>
2018-10-21 00:49:05 +0530

    Added half and-intro

--- Task_2/task_2.cpp
+++ Task_2/task_2.cpp
@@ -2,8 +2,8 @@
 #include <string>
 #include <vector>
 #include <iterator>
-#include <string>
 #include <algorithm>
+#include <sstream> 
 
 #define AND_INTRODUCTION 1
 #define AND_ELIMINATION_1 2
@@ -23,7 +23,9 @@ public:
 	void remove_brackets( string rb);
 	int return_rule(string rule);
 	proof_validator( int l);
-	void removeSpaces();
+	//void removeSpaces();
+	bool check_premise(string prem);
+	bool and_introduction( int line_1, int line_2);
 };
 
 proof_validator:: proof_validator(int l)
@@ -41,21 +43,21 @@ string remove_brackets(string rb)
 }
 
 
-void removeSpaces()
-{
-	for(int i=0;i<k;i++)
-	{
-		int p=0;
-		for(int j=0;j<str[i].length();j++)
-		{
-			if(str[i][j]!=' ')
-			{
-				str[i][p++]=str[i][j];
-			}
-		}
-		str[i].resize(p);
-	}
-}
+// void removeSpaces()
+// {
+// 	for(int i=0;i<k;i++)
+// 	{
+// 		int p=0;
+// 		for(int j=0;j<str[i].length();j++)
+// 		{
+// 			if(str[i][j]!=' ')
+// 			{
+// 				str[i][p++]=str[i][j];
+// 			}
+// 		}
+// 		str[i].resize(p);
+// 	}
+// }
 
 int return_rule(string rule)
 {
@@ -76,6 +78,21 @@ int return_rule(string rule)
 	return -1;
 }
 
+bool proof_validator::check_premise(string prem)
+{
+	if(!(prem.compare("P")))
+		return 1;
+	return 0;
+}
+
+bool proof_validator::and_introduction( int line_1, int line_2)
+{
+	string and_intro_1 = str[line_1];
+	string and_intro_2 = str[line_2];
+	cout << and_intro_1 << " " << and_intro_2 << endl;
+	
+}
+
 int main()
 {	
 	int lines;
@@ -93,12 +110,33 @@ int main()
 	{
 		string temp = *ptr;
 		int index_1 = temp.find('/');
+		string prem = temp.substr(index_1+1,1);
+		if(obj.check_premise(prem))
+		{
+			//cout << "premise found" << endl;
+			continue;
+		}
 		int index_2 = temp.find('/', index_1+1);
 		//cout << index_1 << " " << index_2 << endl;
 		string temp_rule = temp.substr(index_1+1, (index_2-index_1-1));
 		//cout << temp_rule;
 		int rule_index = return_rule(temp_rule);
-		cout << rule_index;
+		if(rule_index == 1)
+		{
+			//(a^b)/^i/1/2
+			int index_3 = (temp.find('/',index_2+1));
+			stringstream var1(temp.substr(index_2+1, index_3-index_2));
+			int line_1 = 0;
+			var1 >> line_1;
+			cout << line_1 << endl;
+			stringstream var2(temp.substr(index_3+1));
+			int line_2 =0;
+			var2 >> line_2;
+			cout << line_2 << endl;
+			obj.and_introduction( line_1-1, line_2-1);
+
+
+		}
 	} 
 	return 0;
 }
