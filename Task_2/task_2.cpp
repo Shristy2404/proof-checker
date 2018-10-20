b6d61b6af142af871a144cf6e9b67af90ff4253b
Shristy2404 <shristykapoor24@gmail.com>
2018-10-21 02:11:31 +0530

    Added comments

--- Task_2/task_2.cpp
+++ Task_2/task_2.cpp
@@ -1,3 +1,4 @@
+//all header files 
 #include <iostream>
 #include <string>
 #include <vector>
@@ -5,6 +6,7 @@
 #include <algorithm>
 #include <sstream> 
 
+//defining macros for rule names 
 #define AND_INTRODUCTION 1
 #define AND_ELIMINATION_1 2
 #define AND_ELIMINATION_2 3
@@ -15,11 +17,14 @@
 
 using namespace std;
 
+//declaring class proof_validator
 class proof_validator
 {
+	//class has a string vector and length of the proof as class variables 
 public:
 	std::vector<std::string> str;
 	int k;
+	int flag; 
 	string remove_brackets( string rb);
 	int return_rule(string rule);
 	proof_validator( int l);
@@ -27,24 +32,24 @@ public:
 	bool check_premise(string prem);
 	bool and_introduction( int line_1, int line_2, string line);
 	string only_statement(string line);
+	void check_rules(int rule_index);
 };
 
+//constructor to assign l to k; initialize proof length value 
 proof_validator:: proof_validator(int l)
 {
 	k=l;
+	flag=0;
 }
 
+//function to remove first and last bracket from the string passed 
 string proof_validator::remove_brackets(string rb)
 {
-	cout << rb << endl;
 	int len = rb.length();
-	cout << len << endl;
 	string temp = rb.substr(1,len-2);
 	rb = temp;
 	rb.resize(len-2);
-	cout << rb << endl;
 	return rb;
-	//(a^(b^c))
 }
 
 int removeSpaces(string &a)
@@ -59,7 +64,16 @@ int removeSpaces(string &a)
 	return count;
 }
 
+//function to return the actual statement before the rule specifications in the proof line	
+string proof_validator::only_statement(string line)
+{
+	string statement;
+	int index = line.find('/');
+	statement = line.substr(0, index);
+	return statement;
+}
 
+//function to return an integer value corresponding to the rule which is to be checked 
 int return_rule(string rule)
 {
 	if(!(rule.compare("^i")))
@@ -79,37 +93,62 @@ int return_rule(string rule)
 	return -1;
 }
 
+void check_rules()
+{
+	vector<string>::iterator ptr;
+
+	for( ptr = obj.str.begin(); ptr!= obj.str.end(); ptr++)
+	{
+		string temp = *ptr;
+		int index_1 = temp.find('/');
+		string prem = temp.substr(index_1+1,1);
+		if(obj.check_premise(prem))
+		{
+			continue;
+		}
+		int index_2 = temp.find('/', index_1+1);
+		string temp_rule = temp.substr(index_1+1, (index_2-index_1-1));
+		int rule_index = return_rule(temp_rule);	
+
+		if(rule_index == 1)
+		{
+			int line_1, line_2 = 0;
+			int index_3 = (temp.find('/',index_2+1));
+			stringstream var1(temp.substr(index_2+1, index_3-index_2));
+			var1 >> line_1;
+			stringstream var2(temp.substr(index_3+1));
+			var2 >> line_2;
+			if(!(obj.and_introduction(line_1-1, line_2-1, temp)))
+			{
+				flag=1;
+				break;
+			}
+		}
+
+}
+
+//function to check if given line of proof is a premise or not 
 bool proof_validator::check_premise(string prem)
 {
 	if(!(prem.compare("P")))
-		return 1;
-	return 0;
-}
-
-bool proof_validator::and_introduction( int line_1, int line_2, string line)
-{
-	string and_intro_1 = only_statement(str[line_1]);
-	string and_intro_2 = only_statement(str[line_2]);
-	string statement_3 = only_statement(line);
-    string statement_4 = remove_brackets(statement_3);
-	string comp_1 = and_intro_1 + "^" + and_intro_2;
-	string comp_2 = and_intro_2 + "^" + and_intro_1;
-	cout << comp_1 << " " << comp_2 << endl;
-	cout << statement_4 << endl;
-	if( (!(statement_4.compare(comp_1))) || (!(statement_4.compare(comp_2))))
 		return true;
 	return false;
-	
 }
-	
 
-string proof_validator::only_statement(string line)
+//function to check and_introduction rule 
+bool proof_validator::and_introduction(int line_1, int line_2, string line)
 {
-	string statement;
-	int index = line.find('/');
-	statement = line.substr(0, index);
-	return statement;
+    string statement_1 = remove_brackets(only_statement(line));
+	string comp_1 = only_statement(str[line_1]) + "^" + only_statement(str[line_2]);
+	string comp_2 = only_statement(str[line_2]) + "^" + only_statement(str[line_1]);
+	if( (!(statement_1.compare(comp_1))) || (!(statement_1.compare(comp_2))))
+		return true;
+	return false;
 }
+
+//function to check and_elimination rule 
+bool proof_validator::and_elimination(int line_1, string line)
+
 
 int main()
 {
@@ -123,40 +162,7 @@ int main()
 		cin >> temp;
 		obj.str.push_back(temp);
 	}
-	vector<string>::iterator ptr;
-
-	for( ptr = obj.str.begin(); ptr!= obj.str.end(); ptr++)
-	{
-		string temp = *ptr;
-		int index_1 = temp.find('/');
-		string prem = temp.substr(index_1+1,1);
-		if(obj.check_premise(prem))
-		{
-			//cout << "premise found" << endl;
-			continue;
-		}
-		int index_2 = temp.find('/', index_1+1);
-		//cout << index_1 << " " << index_2 << endl;
-		string temp_rule = temp.substr(index_1+1, (index_2-index_1-1));
-		//cout << temp_rule;
-		int rule_index = return_rule(temp_rule);
-
-		if(rule_index == 1)
-		{
-			//(a^b)/^i/1/2
-			int line_1, line_2 = 0;
-			int index_3 = (temp.find('/',index_2+1));
-			stringstream var1(temp.substr(index_2+1, index_3-index_2));
-			var1 >> line_1;
-			//cout << line_1 << endl;
-			stringstream var2(temp.substr(index_3+1));
-			var2 >> line_2;
-			//cout << line_2 << endl;
-			if(!(obj.and_introduction( line_1-1, line_2-1, temp)))
-				break;
-
-		}
-	} 
+	
 
 	// for( ptr=obj.str.begin();ptr!=obj.str.end();ptr++)
  //    {
