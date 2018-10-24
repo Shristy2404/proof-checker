84fe7bfe2bc4234bfe4d3221a0bfe1e5ef944dcb
sai sravanthi <sai@localhost.localdomain>
2018-10-24 15:50:09 +0530

    added comments for check rules

--- Task_2/task_2.cpp
+++ Task_2/task_2.cpp
@@ -208,7 +208,7 @@ int proof_validator::check_rules()
 		if(rule_index == 2 || rule_index == 3)
 		{
 			//cout << "and elimination begins" << endl;
-			int line_1 = 0;
+			int line_1 = 0; //!< The index of the line on which and-elimination is performed.
 			stringstream var1(temp.substr(index_2+1));
 			var1 >> line_1;
 			cout << line_1 << endl;
@@ -222,7 +222,7 @@ int proof_validator::check_rules()
 
 		if(rule_index == 4 || rule_index == 5)
 		{
-			int line_1=0;
+			int line_1=0; //!< The index of the line on which or-introduction is performed.
 			stringstream var1(temp.substr(index_2+1));
 			var1>>line_1;
 			cout << line_1 << endl;
